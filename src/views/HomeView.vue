be990ecdc3eb662c8315a65c8483d79f4e19fecd
AsZaychik <aszaychik@gmail.com>
2022-12-25 05:52:22 +0700

    Membuat Data pada Composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="home">
-    <p>Hi {{ name }}, this is your number : {{ number }}</p>
+    <p ref="p">Hi {{ name }},</p>
+    <button @click="handleClick">Show Number!</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
   setup() {
+    const p = ref(null);
     const name = "AsZaychik";
     const number = "089519750202";
 
-    return { name, number };
+    const handleClick = () => {
+      console.log(p, p.value);
+      p.value.classList.add("paragraph");
+      p.value.textContent = `This is your number : ${number}`;
+    };
+
+    return { name, number, p, handleClick };
   },
 };
 </script>
+
+<style scoped>
+.paragraph {
+  color: red;
+}
+</style>
