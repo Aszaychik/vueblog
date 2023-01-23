d43b2b07102fba6665d2c83873b36a933e8df3e4
aszaychik <aszaychik@gmail.com>
2023-01-23 20:31:18 +0700

    Manipulasi Data Dengan Computed Property

---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <input type="text" v-model="search" />
+    <div v-for="(name, index) in resultSearch" :key="index">
+      {{ name }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from "vue";
+export default {
+  name: "Home",
+  setup() {
+    // const name = computed(() => {
+    //   return "AsZaychik";
+    // });
+
+    const search = ref("");
+
+    const names = ref([
+      "Emily",
+      "Michael",
+      "Madison",
+      "Matthew",
+      "Daniel",
+      "Jessica",
+      "Nicholas",
+      "Ashley",
+      "Sarah",
+      "Brandon",
+      "Alyssa",
+      "Jacob",
+      "Samantha",
+      "William",
+      "Jennifer",
+      "Joseph",
+      "Amanda",
+      "Joshua",
+      "Stephanie",
+      "Andrew",
+      "Lauren",
+      "David",
+      "Rachel",
+      "Anthony",
+    ]);
+
+    const resultSearch = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    return { search, names, resultSearch };
+  },
+};
+</script>
